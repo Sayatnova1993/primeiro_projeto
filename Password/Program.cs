402784bec315cb30bbe7caaed2b7b78412779a0f
Lucas Morabito <lucas.morabito@gmail.com>
2020-05-26 19:12:25 -0300

    Working version

---
+++ Password/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace Password
+{
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+            string input;
+            string password = "2002";
+
+            while (true)
+            {
+                input = Console.ReadLine();
+                if (input == password)
+                {
+                    Console.WriteLine("Senha Correta.");
+                    break;
+                }
+                else
+                {
+                    Console.WriteLine("Senha Inválida");
+                }
+
+            }
+        }
+    }
+}
